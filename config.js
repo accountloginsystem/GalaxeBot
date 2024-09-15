75cb577c18d4137f5f26c60a552f59a5abba2fbe
accountloginsystem <168574688+accountloginsystem@users.noreply.github.com>
2024-09-15 16:33:15 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // config.js
 module.exports = {
   BOT_TOKEN: '6503615948:AAFvtlREHb3o_R69zkJorAYu94JbDYJcdGY',
-  WEB_APP_URL: 'https://hammerhead-app-gaj34.ondigitalocean.app'
+  WEB_APP_URL: 'https://coral-app-avvyd.ondigitalocean.app/'
 };
